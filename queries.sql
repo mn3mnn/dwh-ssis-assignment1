17a20b88229e95159e309a7c581143ae8cac78b3
monem <abdoeladawy4@gmail.com>
2025-04-16 13:26:16 +0200

    add init sln

---
+++ queries.sql
@@ -0,0 +1,18 @@
+USE [fcai_dwh]
+GO
+
+/****** Object:  Table [dbo].[Employee_Q2]    Script Date: 4/14/2025 9:03:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employee_Q2](
+	[Name] [varchar](50) NULL,
+	[City] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+	[Update_Date] [datetime] NULL
+) ON [PRIMARY]
+GO
+
