1bea63f353c71cbc16635e45e31ecc3ff2535dff
monem <abdoeladawy4@gmail.com>
2025-04-18 16:56:49 +0200

    add Q4

--- queries.sql
+++ queries.sql
@@ -80,4 +80,67 @@ SELECT * FROM [dbo].[Dim_Employee_Q3];
 
 
 /********************************************************************************************************************/
+/****** 
+Question 4
+******/
+/********************************************************************************************************************/
 
+
+
+USE [fcaidb]
+GO
+
+CREATE TABLE Attendance_Device (
+    ID INT PRIMARY KEY,                   
+    Employee_Id INT NOT NULL,             
+    Finger_Print_TS DATETIME NOT NULL,  
+    In_Out VARCHAR(10) 
+);
+
+
+INSERT INTO Attendance_Device(ID, Employee_Id, Finger_Print_TS, In_Out)
+VALUES
+    (1, 101, '2025-03-12 09:00:00', 'in'),
+    (2, 101, '2025-03-12 10:00:00', 'in'),
+    (3, 102, '2025-03-12 09:00:00', 'in'),
+    (4, 103, '2025-03-12 11:00:00', 'in'),
+    (5, 104, '2025-03-12 09:15:00', 'in'),
+    (6, 105, '2025-03-12 10:00:00', 'in'),
+    (7, 105, '2025-03-12 11:00:00', 'in'),
+    (8, 105, '2025-03-12 11:30:00', 'in'),
+    (9, 106, '2025-03-12 09:00:00', 'in'),
+    (10, 107, '2025-03-12 09:00:00', 'in'),
+    (11, 108, '2025-03-12 09:00:00', 'in'),
+    (12, 101, '2025-03-12 09:00:00', 'out'),
+    (13, 101, '2025-03-12 17:00:00', 'out'),
+    (14, 101, '2025-03-12 19:00:00', 'out'),
+    (15, 102, '2025-03-12 17:00:00', 'out'),
+    (16, 103, '2025-03-12 17:00:00', 'out'),
+    (17, 105, '2025-03-12 10:00:00', 'out'),
+    (18, 105, '2025-03-12 11:00:00', 'out'),
+    (19, 105, '2025-03-12 18:00:00', 'out'),
+    (20, 106, '2025-03-12 19:00:00', 'out'),
+    (21, 107, '2025-03-12 14:00:00', 'out'),
+    (22, 108, '2025-03-12 17:00:00', 'out');
+
+
+	
+USE [fcaidwh]
+GO
+
+CREATE TABLE Employee_Attendance_Details (
+    Att_Key INT identity(1,1) PRIMARY KEY,              
+    Emp_ID INT NOT NULL,                  
+    [Date] DATE NOT NULL,                   
+    Time_In TIME(0) NOT NULL,             
+    Time_Out TIME(0),            
+    Worked_Hours INT,            
+    [State] VARCHAR(50) NOT NULL          
+);
+
+
+USE [fcaidwh]
+GO
+select * from [dbo].[Employee_Attendance_Details] order by Emp_ID;
+
+delete from [dbo].[Employee_Attendance_Details];
